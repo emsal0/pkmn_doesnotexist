611013f9e5ca1b6d0b35f2b9a46938731db932a7
Calvin Liang <me@calvin.sh>
2020-01-11 14:38:32 -0800

    [web] Update page

---
+++ web/public/index.js
@@ -0,0 +1,14 @@
+// const model = await tf.loadLayersModel("http://model-server.domain/download/model.json");
+
+// model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+// // Train the model using the data.
+// model.fit(xs, ys, {epochs: 10}).then(() => {
+//   // Use the model to do inference on a data point the model hasn't seen before:
+//   model.predict(tf.tensor2d([5], [1, 1])).print();
+//   // Open the browser devtools to see the output
+// });
+
+document.querySelector("#generate-button").onclick = () => {
+  console.log("daiushdof");
+};
