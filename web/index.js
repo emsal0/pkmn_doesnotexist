0aeb844e72e93d0209c02ff216e2c2e743fed40b
Calvin Liang <me@calvin.sh>
2020-01-12 00:47:35 -0800

    Update index.js

--- web/index.js
+++ web/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
+const { spawn } = require("child_process");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const uri =
   "mongodb+srv://dbUser:dbPass@cluster0-u1ctf.gcp.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -27,6 +28,21 @@ app.use(express.static(__dirname + "/public"));
  */
 app.post("/api/set-name", function(req, res) {
   pkmnNamesCollection.insertOne({ name: req.body.name });
+});
+
+app.post("/api/get-image", function(req, res) {
+  const types = [];
+
+  if (req.body.type1) types.push(req.body.type1);
+  if (req.body.type1) types.push(req.body.type2);
+
+  const process = spawn("python", ["../get_pokemon.py", ...types]);
+
+  process.stdout.on("data", function(data) {
+    // res.send(data.toString());
+  });
+
+  // pkmnNamesCollection.insertOne({ name: req.body.name });
 });
 
 app.listen(PORT, function() {
