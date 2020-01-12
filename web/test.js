a733db29ed82b67a7ea93f46df2b732d70d14a73
Calvin Liang <me@calvin.sh>
2020-01-12 08:35:40 -0800

    Delete test.js

--- web/test.js
+++
@@ -1,7 +0,0 @@
-const { spawn } = require("child_process");
-
-const process = spawn("python", ["../ml/get_pokemon.py", "normal"]);
-
-process.stdout.on("data", function(data) {
-  console.log(data.toString());
-});
