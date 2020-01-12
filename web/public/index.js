a7de384a508f7f4cbac0e66e018eadb9f48e665f
Calvin Liang <me@calvin.sh>
2020-01-11 19:01:12 -0800

    [web] Add tensorflow for js

--- web/public/index.js
+++ web/public/index.js
@@ -1,14 +1,3 @@
-// const model = await tf.loadLayersModel("http://model-server.domain/download/model.json");
-
-// model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-// // Train the model using the data.
-// model.fit(xs, ys, {epochs: 10}).then(() => {
-//   // Use the model to do inference on a data point the model hasn't seen before:
-//   model.predict(tf.tensor2d([5], [1, 1])).print();
-//   // Open the browser devtools to see the output
-// });
-
 const namingButton = document.querySelector("#naming-button");
 const namingInputContainer = document.querySelector("#naming-input-container");
 const namingInput = document.querySelector("#naming-input");
@@ -48,3 +37,14 @@ function reset() {
 
   namingInputContainer.classList.remove("editable");
 }
+
+async function useModel() {
+  const model = await tf.loadGraphModel("web_model/model.json");
+  const cat = document.getElementById("cat");
+
+  const randomNoise = tf.randomNormal([1, 100]);
+
+  model.predict(randomNoise);
+}
+
+useModel();
