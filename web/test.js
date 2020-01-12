75ad8c5b38bc35e0242bb0ed7156fc9c6de74ad5
Calvin Liang <me@calvin.sh>
2020-01-12 08:32:19 -0800

    Update test.js

--- web/test.js
+++ web/test.js
@@ -1,6 +1,6 @@
 const { spawn } = require("child_process");
 
-const process = spawn("python", ["../get_pokemon.py", "normal"]);
+const process = spawn("python", ["../ml/get_pokemon.py", "normal"]);
 
 process.stdout.on("data", function(data) {
   console.log(data.toString());
